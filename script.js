fec3f99d695191ba0bd66bfb016e30c8cce9aaa9
Erin Sparling <erin@everyplace.net>
2013-05-09 20:29:46 -0400

    Revert "Messed up commit."
    This reverts commit 1506993e432720f66c1bfb45e623c3250810efe4.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
 	$(".contentwide").addClass("grid");
 
-=======
-	$("<div id='omgInjected' />").appendTo("body");
->>>>>>> parent of a95fe72... width contorls
 	console.log("done");
 });
