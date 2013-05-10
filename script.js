1506993e432720f66c1bfb45e623c3250810efe4
Erin Sparling <erin@everyplace.net>
2013-05-09 20:29:35 -0400

    Messed up commit.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
+<<<<<<< HEAD
 	$(".contentwide").addClass("grid");
 
+=======
+	$("<div id='omgInjected' />").appendTo("body");
+>>>>>>> parent of a95fe72... width contorls
 	console.log("done");
 });
